75dece500d908e304247fb4b3a12fac109a80051
vsiisv <shores.freezer0u@icloud.com>
2025-10-18 11:13:20 +1000

    Get user input

--- main.py
+++ main.py
@@ -4,8 +4,26 @@ def can_vote(age, citizen, criminal_record):
     else:
         print("You can't vote!")
 
-age = 18
-citizen = True
-criminal_record = False
+def get_age():
+    while True:
+        message = input("Введите ваш возраст: ")
+        try:
+            age = int(message)
+            return age
+        except ValueError:
+            print("Введите числовое значение.")
 
-can_vote(age, citizen, criminal_record)
+def get_data(user_message):
+    operator = input(user_message).lower()
+    while operator not in ["да", "нет"]:
+        print("Введен некорректный ответ")
+        operator = input(user_message).lower()
+    return True if operator == "да" else False
+
+def main():
+    age = get_age()
+    citizen = get_data("Вы являетесь гражданином страны где сейчас находитесь (Да/Нет): ")
+    criminal_record = get_data("У вас есть судимость (Да/Нет): ")
+    can_vote(age, citizen, criminal_record)
+
+main()
